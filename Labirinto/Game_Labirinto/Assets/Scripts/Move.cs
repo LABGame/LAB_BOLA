eb7156208706f62725cf60d1efaaea20bac7b64c
Cauan Cordeiro Santana <cauansantana@hotmail.com>
2020-04-06 02:47:01 -0300

    Pacote_Unity
    Primeiro pacote de informação da unity

---
+++ Labirinto/Game_Labirinto/Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    private Rigidbody Bola;
+ 
+    void Start()
+    {
+        //Bola = gameObject.AddComponent<Rigidbody>();
+        //Bola.mass = 1.0f;
+        //Physics.gravity = new Vector3(0, -10f, 0);
+        //Bola.drag = -0.1f;
+        //Bola.angularDrag = 0f;
+        Bola.velocity = new Vector3(10.0f,0, 10.0f);
+
+    }
+
+    void Update()
+    {
+        //Bola.WakeUp();
+
+    }
+}
---
+++ Labirinto/Game_Labirinto/Assets/Scripts/Rotacao.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao : MonoBehaviour
+{
+    //Ang
+    public float xRot_Tab;
+    public float yRot_Tab;
+    public float zRot_Tab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotacao_Tabuleiro
+        RotacaoTab();
+        InputDeRotacaoX();
+        InputDeRotacaoZ();
+
+     }
+
+    //Metodo para preset dos vetores de rotacao do tabuleiro
+    void RotacaoTab()
+    {
+       //transform.rotation = Quaternion.Euler(xRot_Tab,0,zRot_Tab);
+       transform.eulerAngles = new Vector3(xRot_Tab,0,zRot_Tab);
+    }
+    
+    //Reconhece o pressionamento do Botao e Rotaciona no eixo X
+    void InputDeRotacaoX()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            xRot_Tab += 8f*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            xRot_Tab -= 8f*Time.deltaTime;
+        }
+    }
+
+    //Reconhece o pressionamento do Botao e Rotaciona no eixo Z
+    void InputDeRotacaoZ()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            zRot_Tab -= 8f*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            zRot_Tab += 8f*Time.deltaTime;
+        }
+    }
+}
